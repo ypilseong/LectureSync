326b3b95f59cba0f5f6afdcda4522435bd2e0967
Yang pilseong <lal4995@gmail.com>
2024-06-06 21:51:51 +0900

    feat: fix rag, summarize,streamlit_frontend.py by ypilseong

--- src/lecturesync/langchain/rag.py
+++ src/lecturesync/langchain/rag.py
@@ -1,70 +1,78 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain_community.vectorstores import Chroma, FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain_community.chat_models import ChatOllama
 
 class Chatbot:
-    def __init__(self, pdf_path, txt_path):
+    def __init__(self, pdf_path=None, txt_path=None, txt = None):
         self.pdf_path = pdf_path
         self.txt_path = txt_path
+        self.txt = txt
+        self.docs = []
+        if pdf_path:
+            for file_path in pdf_path:    
+                pdf_loader = PyPDFLoader(file_path)
+                self.docs += pdf_loader.load()
+        if txt_path:
+            for file_path in txt_path:
+                txt_loader = TextLoader(file_path)
+                self.docs += txt_loader.load()
+        if txt:
+            self.docs += self.txt
 
-        # 단계 1: 문서 불러오기(docs load)
-        pdf_loader = PyPDFLoader(self.pdf_path)
-        txt_loader = TextLoader(self.txt_path)
+        self.vectorstore = None
+        self.retriever = None
+        if self.docs:
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+            self.splits = text_splitter.split_documents(self.docs)
 
-        pdf_docs = pdf_loader.load()
-        txt_docs = txt_loader.load()
+            self.vectorstore = FAISS.from_documents(documents=self.splits, embedding=HuggingFaceBgeEmbeddings())
+            self.retriever = self.vectorstore.as_retriever()
 
-        self.docs = pdf_docs + txt_docs
+        self.chat_history = []
 
-        # 단계 2: 문서 분할(Split Documents)
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
-        self.splits = text_splitter.split_documents(self.docs)
-
-        # 단계 3: 임베딩 & 벡터스토어 생성(Create Vectorstore)
-        self.vectorstore = FAISS.from_documents(documents=self.splits, embedding=HuggingFaceBgeEmbeddings())
-
-        # 단계 4: 검색(Search)
-        self.retriever = self.vectorstore.as_retriever()
-
-        # 단계 5: 프롬프트 생성(Create Prompt)
         self.prompt = ChatPromptTemplate.from_messages([
-            ("system", "친절한 챗봇으로서 상대방의 요청에 최대한 자세하고 친절하게 답하자. 대답은 다음 context 기반으로 대답해. 모든 대답은 한국어(Korean)으로 대답해줘. Context: {context} "),
+            ("system", "친절한 챗봇으로서 상대방의 요청에 최대한 자세하고 친절하게 답하자. 대답은 다음 context 기반으로 대답해. 모든 대답은 한국어(Korean)으로 대답해줘."),
             ("human", "{question}")
         ])
 
-        # 단계 6: 언어모델 생성(Create LLM)
-        tokenizer = AutoTokenizer.from_pretrained("beomi/Llama-3-Open-Ko-8B-Instruct-preview", padding_side='left')
-        model = AutoModelForCausalLM.from_pretrained("beomi/Llama-3-Open-Ko-8B-Instruct-preview")
-
-        pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=2048, truncation=True, device=0)
-        self.gpu_llm = HuggingFacePipeline(
-            pipeline=pipe,
-            batch_size=2,  # 배치 크기 조정
-            pipeline_kwargs={
-                "max_new_tokens": 512,
-                "max_length": 1024,
-                "do_sample": True,
-                "temperature": 0.5
-            }
-        )
+        self.llm = ChatOllama(base_url='http://172.16.229.33:11436',
+                              model='EEVE-Korean-Instruct-10.8B')
 
     @staticmethod
     def format_docs(docs):
-        # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
         return "\n\n".join(doc.page_content for doc in docs)
 
+    def add_to_history(self, question, response):
+        self.chat_history.append(("human", question))
+        self.chat_history.append(("assistant", response))
+
     def create_chain(self):
-        # 단계 7: 체인 생성(Create Chain)
-        rag_chain = (
-            {"context": self.retriever | self.format_docs, "question": RunnablePassthrough()}
-            | self.prompt
-            | self.gpu_llm
-            | StrOutputParser()
-        )
+        if self.retriever:
+            rag_chain = (
+                {"context": self.retriever | self.format_docs, "question": RunnablePassthrough()}
+                | self.prompt
+                | self.llm
+                | StrOutputParser()
+            )
+        else:
+            rag_chain = (
+                {"context": RunnablePassthrough(), "question": RunnablePassthrough()}
+                | self.prompt
+                | self.llm
+                | StrOutputParser()
+            )
         return rag_chain
+
+    def chat(self, question):
+        context = "\n".join([f"{role}: {text}" for role, text in self.chat_history])
+        inputs = {"context": context, "question": question}
+        chain = self.create_chain()
+        response = chain.invoke(inputs)
+        self.add_to_history(question, response["text"])
+        return response["text"]
+
--- src/lecturesync/langchain/summarize.py
+++ src/lecturesync/langchain/summarize.py
@@ -1,126 +1,108 @@
+import os
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.llm import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain
-from langchain.chains.combine_documents.map_reduce import ReduceDocumentsChain
-import os
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.combine_documents.map_reduce import MapReduceDocumentsChain, ReduceDocumentsChain
+from langchain_community.chat_models import ChatOllama
+from langchain_community.chat_models import ChatOpenAI
+class DocumentSummarizer:
+    def __init__(self, pdf_path, model_url, model_name, temperature=0, chunk_size=2000, chunk_overlap=200):
+        self.pdf_path = pdf_path
+        self.model_url = model_url
+        self.model_name = model_name
+        self.temperature = temperature
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        
+        # 설정된 환경 변수
+        os.environ["TRANSFORMERS_CACHE"] = "./data/llm_model/"
+        os.environ["HF_HOME"] = "./data/llm_model/"
 
-from langchain_core.output_parsers import StrOutputParser
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+    def load_documents(self):
+        for file_path in self.pdf_path:
+            loader = PyPDFLoader(file_path)
+        return loader.load()
 
-# ./cache/ 경로에 다운로드 받도록 설정
-os.environ["TRANSFORMERS_CACHE"] = "./data/llm_model/"
-os.environ["HF_HOME"] = "./data/llm_model/"
+    def split_documents(self, docs):
+        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+            separator="\n\n",
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap,
+        )
+        return text_splitter.split_documents(docs)
 
-def summarization():
-    tokenizer = AutoTokenizer.from_pretrained("beomi/Llama-3-Open-Ko-8B-Instruct-preview",
-                                              padding_side='left',
-                                              truncation=True)
-    model = AutoModelForCausalLM.from_pretrained("beomi/Llama-3-Open-Ko-8B-Instruct-preview")
+    def create_llm(self):
+        return ChatOllama(base_url=self.model_url,
+                      model=self.model_name,
+                      temperature=self.temperature)
+        # return ChatOpenAI(model_name='gpt-3.5-turbo-0125')
 
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer,max_length=2048,truncation=True,pad_token_id=tokenizer.eos_token_id,device=0)
-    gpu_llm = HuggingFacePipeline(
-        pipeline = pipe,
-        batch_size=2,  # 배치 크기s를 조정합니다. GPU 메모리와 모델 크기에 따라 적절히 설정합니다.
-        pipeline_kwargs={
-            "do_sample": True,
-            "temperature": 0.5
-        }  # 모델에 전달할 추가 인자를 설정합니다.
-    )
+    def create_map_reduce_chain(self, llm):
+        # Define map prompt
+        map_prompt_template = """다음은 문서(CONTEXT) 중 일부 내용입니다. 이 문서 목록을 기반으로 주요 내용을 한국어(Korean)로 요약해:
+        너는 다음과 같은 규칙을 따라야돼.
+        - 특수기호 제외
+        - 문장에 대한 연관성이 있음
+        - 핵심 내용에 대한 언급
+        - 글자만 생성해
+        CONTEXT:
+        {context}
+        
+        답변:"""
+        map_prompt = PromptTemplate.from_template(map_prompt_template)
 
-    
+        # Define reduce prompt
+        reduce_template = """다음은 요약의 집합입니다. 이것(CONTEXT)들을 바탕으로 통합된 요약을 자연스러운 문장으로 한국말(Korean)로 만들어.
+        너는 다음과 같은 규칙을 따라야돼.
+        - 특수기호 제외
+        - 문장에 대한 연관성이 있음
+        - 핵심 내용에 대한 언급
+        - 글자만 생성해
+        CONTEXT:
+        {context}
+        
+        답변:"""
+        reduce_prompt = PromptTemplate.from_template(reduce_template)
 
-    loader = PyPDFLoader('data/doc_data/머신러닝을활용한가짜리뷰탐지연구사용자행동분석을중심으로.pdf')
-    docs = loader.load()
-    
-    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n\n",  # 분할기준
-        chunk_size=700,   # 사이즈
-        chunk_overlap=200, # 중첩 사이즈
-    )
-    splits = text_splitter.split_documents(docs)
-    
-    print(f'총 분할된 도큐먼트 수: {len(splits)}')
+        # Define chains
+        map_chain = LLMChain(llm=llm, prompt=map_prompt)
+        reduce_chain = LLMChain(llm=llm, prompt=reduce_prompt)
+        
+        document_prompt = PromptTemplate(
+            input_variables=["page_content"],
+            template="{page_content}"
+        )
+        
+        combine_documents_chain = StuffDocumentsChain(
+            llm_chain=reduce_chain,
+            document_prompt=document_prompt,
+            document_variable_name="context"
+        )
 
-    # Define prompt
-    prompt_template = """다음은 문서 중 일부 내용입니다.
-    {pages}
-    이 문서 목록을 기반으로 주요 내용을 요약해 주세요.
-    답변:"""
-    prompt = PromptTemplate.from_template(prompt_template)
+        reduce_documents_chain = ReduceDocumentsChain(
+            combine_documents_chain=combine_documents_chain,
+            collapse_documents_chain=combine_documents_chain,
+            token_max=8000,
+        )
 
-    # 프롬프트와 언어 모델을 연결하여 체인을 생성합니다. gpu_llm.bind(stop=["\n\n"])
-    #map_chain = prompt | gpu_llm | StrOutputParser()
-    map_chain = LLMChain(llm=gpu_llm, prompt=prompt)
+        return MapReduceDocumentsChain(
+            llm_chain=map_chain,
+            reduce_documents_chain=reduce_documents_chain,
+            document_variable_name="context",
+        )
 
-    reduce_template = """다음은 요약의 집합입니다:
-    {doc_summaries}
-    이것들을 바탕으로 통합된 요약을 만들어 주세요.
-    답변:"""
+    def summarize(self):
+        docs = self.load_documents()
+        splits = self.split_documents(docs)
+        print(f'총 분할된 도큐먼트 수: {len(splits)}')
 
-    # Reduce 프롬프트 완성
-    reduce_prompt = PromptTemplate.from_template(reduce_template)
+        llm = self.create_llm()
+        map_reduce_chain = self.create_map_reduce_chain(llm)
 
-    # Reduce에서 수행할 LLMChain 정의
-    #reduce_chain = reduce_prompt | gpu_llm | StrOutputParser()
-    reduce_chain = LLMChain(llm=gpu_llm, prompt=reduce_prompt)
-    
-
-    document_prompt = PromptTemplate(
-                input_variables=["page_content"],
-                template="{page_content}"
-    )
-    # 문서의 목록을 받아들여, 이를 단일 문자열로 결합하고, 이를 LLMChain에 전달합니다.
-    combine_documents_chain = StuffDocumentsChain(
-        llm_chain=reduce_chain,
-        document_prompt=document_prompt,
-        document_variable_name="doc_summaries"
-    )
+        result = map_reduce_chain.invoke({"input_documents":splits}, return_only_outputs=True)
+        print(result)
 
 
-    # Map 문서를 통합하고 순차적으로 Reduce합니다.
-    reduce_documents_chain = ReduceDocumentsChain(
-        # 호출되는 최종 체인입니다.
-        combine_documents_chain=combine_documents_chain,
-        # 문서가 `StuffDocumentsChain`의 컨텍스트를 초과하는 경우
-        collapse_documents_chain=combine_documents_chain,
-        # 문서를 그룹화할 때의 토큰 최대 개수입니다.
-        token_max=4000,
-    )
-
-    # ========== ⑤ Map-Reduce 통합단계 ========== #
-
-    # 문서들에 체인을 매핑하여 결합하고, 그 다음 결과들을 결합합니다.
-    map_reduce_chain = MapReduceDocumentsChain(
-        # Map 체인
-        llm_chain=map_chain,
-        # Reduce 체인
-        reduce_documents_chain=reduce_documents_chain,
-        # 문서를 넣을 llm_chain의 변수 이름(map_template 에 정의된 변수명)
-        document_variable_name="pages",
-        # 출력에서 매핑 단계의 결과를 반환합니다.
-        return_intermediate_steps=False,
-    )
-
-    # # Define StuffDocumentsChain
-    # summaries=[]
-    # for split in splits:
-    #     response = gpu_chain.invoke({"text": split.page_content})
-    #     summaries.append(response)
-    #     print(response)
-    
-    # final_summary = "\n\n".join(summaries)
-    # print(final_summary)    
-
-    # with open('data/summariztion/example.txt', 'w', encoding='utf-8') as file:
-    # # 변수의 내용을 파일에 쓰기
-    #     file.write(final_summary)
-
-    # return response
-    result = map_reduce_chain.invoke(splits)
-    print(result)
-
-summarization()
+        return result['output_text']
